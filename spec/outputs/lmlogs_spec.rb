20ae890c8bc0ad6e87423c98f92af41f87491b4c
Siddharth Kulkarni <siddharth.kulkarni98@gmail.com>
2021-03-19 15:35:53 +0530

    unit tests, no need of metadata and flatten

--- spec/outputs/lmlogs_spec.rb
+++ spec/outputs/lmlogs_spec.rb
@@ -12,7 +12,8 @@ describe LogStash::Outputs::LMLogs do
       "portal_name" => "localhost",
       "access_id" => "access_id",
       "access_key" => "access_key",
-      "batch_size" => 3
+      "batch_size" => 3,
+      "lm_property" => "test.property"
       )
     @lmlogs.register
     allow(@lmlogs).to receive(:client).and_return(client)
@@ -23,29 +24,28 @@ describe LogStash::Outputs::LMLogs do
     allow(@lmlogs).to receive(:client).and_return(client)
   end
 
-  #TODO: WRITE TESTS THAT WORK I HATE THIS
-  #it "Forwards an event" do
-  #    expect(client).to receive(:post).once.and_call_original
-  #    @lmlogs.multi_receive([sample_event])
-  #end
+  it "Forwards an event" do
+      expect(client).to receive(:post).once.and_call_original
+      @lmlogs.multi_receive([sample_event])
+  end
 
-  #it "Batches multiple events and extracts metadata" do
-  #  event1 = LogStash::Event.new("message" => "hello this is log 1", "host" => "host1")
-  #  event2 = LogStash::Event.new("message" => "hello this is log 2", "host" => "host2")
-  #  event3 = LogStash::Event.new("message" => "hello this is log 3", "host" => "host3")
-  #  expect(client).to receive(:post).once.with("localhost/v1/batch",hash_including(:body => LogStash::Json.dump(
-  #      [{"message" => "hello this is log 1", "source" => "host1", "timestamp" => event1.timestamp.to_s, "metadata" => {"@version" => "1"}},
-  #      {"message" => "hello this is log 2", "source" => "host2", "timestamp" => event2.timestamp.to_s, "metadata" => {"@version" => "1"}},
-  #      {"message" => "hello this is log 3", "source" => "host3", "timestamp" => event3.timestamp.to_s, "metadata" => {"@version" => "1"}}
-  #      ]
-  #      ))).and_call_original
-  #  @lmlogs.multi_receive([event1, event2, event3])
-  #end
+  it "Batches multiple events and extracts metadata" do
+    event1 = LogStash::Event.new("message" => "hello this is log 1", "host" => "host1")
+    event2 = LogStash::Event.new("message" => "hello this is log 2", "host" => "host2")
+    event3 = LogStash::Event.new("message" => "hello this is log 3", "host" => "host3")
+    expect(client).to receive(:post).once.with("https://localhost.logicmonitor.com/rest/log/ingest",hash_including(:body => LogStash::Json.dump(
+        [{"message" => "hello this is log 1", "_lm.resourceId" => {"test.property" => "host1"}, "timestamp" => event1.timestamp.to_s},
+        {"message" => "hello this is log 2", "_lm.resourceId" => {"test.property" => "host2"}, "timestamp" => event2.timestamp.to_s},
+        {"message" => "hello this is log 3", "_lm.resourceId" => {"test.property" => "host3"}, "timestamp" => event3.timestamp.to_s}
+        ]
+        ))).and_call_original
+    @lmlogs.multi_receive([event1, event2, event3])
+  end
 
-  #it "Batches data of size batch_size" do
-  #  expect(client).to receive(:post).exactly(2).times.and_call_original
-  #  @lmlogs.multi_receive([sample_event, sample_event, sample_event, sample_event])
-  #end
+  it "Batches data of size batch_size" do
+    expect(client).to receive(:post).exactly(2).times.and_call_original
+    @lmlogs.multi_receive([sample_event, sample_event, sample_event, sample_event, sample_event])
+  end
 
 
 end
